ebf52002b032bbe1f7e3e26d9e45884d478b2bdb
Chithara <chithara.2019160@iit.ac.lk>
2021-10-26 21:27:48 +0530

    added driver's stats

--- src/CarChampionship/Formula1Driver.java
+++ src/CarChampionship/Formula1Driver.java
@@ -1,7 +1,15 @@
 package CarChampionship;
 
 public abstract class Formula1Driver {
+    //Basic Information
     private String name;
     private String location;
     private String team;
+    //Driver's statistics
+    private int firstPositionCount;
+    private int secondPositionCount;
+    private int thirdPositionCount;
+    private int points;
+
+
 }
