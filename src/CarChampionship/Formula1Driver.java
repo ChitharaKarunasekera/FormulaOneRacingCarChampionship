e2ad9cdee3ee64bb9b8e8e5c66a804bb59a8d095
Chithara <chithara.2019160@iit.ac.lk>
2021-10-26 21:37:04 +0530

    added getters and setters

--- src/CarChampionship/Formula1Driver.java
+++ src/CarChampionship/Formula1Driver.java
@@ -10,6 +10,80 @@ public abstract class Formula1Driver {
     private int secondPositionCount;
     private int thirdPositionCount;
     private int points;
+    private int racesCount;
+
+    //Getter Methods
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public int getFirstPositionCount() {
+        return firstPositionCount;
+    }
+
+    public int getSecondPositionCount() {
+        return secondPositionCount;
+    }
+
+    public int getThirdPositionCount() {
+        return thirdPositionCount;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getRacesCount(){
+        return racesCount;
+    }
 
 
+    //Setter Methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public void setFirstPositionCount(int firstPositionCount) {
+        this.firstPositionCount = firstPositionCount;
+    }
+
+    public void setSecondPositionCount(int secondPositionCount) {
+        this.secondPositionCount = secondPositionCount;
+    }
+
+    public void setThirdPositionCount(int thirdPositionCount) {
+        this.thirdPositionCount = thirdPositionCount;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public void setRacesCount(int count){
+        this.racesCount = count;
+    }
+
+
+    public void calculateTotPoints(){
+    }
+
+    public void assigningPoints(){
+        
+    }
 }
