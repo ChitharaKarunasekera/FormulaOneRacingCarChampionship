cb88c80fb48b96f39ef7d286e9a3a53ed9772a11
Chithara <chithara.2019160@iit.ac.lk>
2021-11-03 19:04:34 +0530

    Added new class and Interface

---
+++ src/CarChampionship/Formula1ChampionshipManager.java
@@ -0,0 +1,49 @@
+package CarChampionship;
+import java.util.ArrayList;
+import java.util.SplittableRandom;
+
+public class Formula1ChampionshipManager implements ChampionshipManager{
+
+    ArrayList<Formula1Driver> drivers = new ArrayList<Formula1Driver>();//All divers participating in championship
+
+
+    @Override
+    public void addNewDriver() {
+        
+    }
+
+    @Override
+    public void deleteDriver() {
+
+    }
+
+    @Override
+    public void changeTeam() {
+
+    }
+
+    @Override
+    public void displayStatistics() {
+
+    }
+
+    @Override
+    public void displayAllDrivers() {
+
+    }
+
+    @Override
+    public void raceStatistics() {
+
+    }
+
+    @Override
+    public void saveToFile() {
+
+    }
+
+    @Override
+    public void readFromFile() {
+
+    }
+}
