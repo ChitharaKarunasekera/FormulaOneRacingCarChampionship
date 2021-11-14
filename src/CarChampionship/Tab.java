6ebc83d8a653f407a4af1c7ff30743d9ea54aff9
Chithara <chithara.2019160@iit.ac.lk>
2021-11-14 19:36:50 +0530

    made changes

--- src/CarChampionship/Tab.java
+++ src/CarChampionship/Tab.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Tab extends JFrame {
-    JTable table;
+    JFrame myFrame;
 
     public Tab() {
         setLayout(new FlowLayout());
@@ -16,18 +16,8 @@ public class Tab extends JFrame {
 
         Object[][] driverStats = new Object[5][7];
 
-        for (int i = 0; i < 5 - 1; i++) {
-            driverStats[i][0] = 0;//driver.getName();
-            driverStats[i][1] = 0;//driver.getTeam();
-            driverStats[i][2] = 0;//driver.getFirstPositionCount();
-            driverStats[i][3] = 0;//driver.getSecondPositionCount();
-            driverStats[i][4] = 0;//driver.getThirdPositionCount();
-            driverStats[i][5] = 0;//driver.getPoints();
-            driverStats[i][6] = 0;//driver.getRacesCount();
-        }
-
 //        for (Formula1Driver driver : Formula1ChampionshipManager.drivers) {
-        for (int i = 0; i < 5 - 1; i++) {
+        for (int i = 0; i < 5; i++) {
             driverStats[i][0] = 0;//driver.getName();
             driverStats[i][1] = 0;//driver.getTeam();
             driverStats[i][2] = 0;//driver.getFirstPositionCount();
@@ -38,16 +28,6 @@ public class Tab extends JFrame {
         }
 //        }
 
-        for (int i = 0; i < 5 - 1; i++) {
-            System.out.print(driverStats[i][0] + " ");
-            System.out.print(driverStats[i][1] + " ");
-            System.out.print(driverStats[i][2] + " ");
-            System.out.print(driverStats[i][3] + " ");
-            System.out.print(driverStats[i][4] + " ");
-            System.out.print(driverStats[i][5] + " ");
-            System.out.print(driverStats[i][6] + " ");
-        }
-        //System.out.println("\n");
 
 //        Object[][] data = {
 //                {"Bill", "Hazel", "Male", "Male", "Male", "Male", 0},
@@ -56,7 +36,7 @@ public class Tab extends JFrame {
 //                {"Janice", "Yellow", "Female", "Male", "Male", "Male", 0},
 //        };
 
-        table = new JTable(driverStats, columnNames);// creating table with column names and its data
+        JTable table = new JTable(driverStats, columnNames);// creating table with column names and its data
         table.setPreferredScrollableViewportSize(new Dimension(600, 150));//width and height of table
         table.setFillsViewportHeight(true);
 
