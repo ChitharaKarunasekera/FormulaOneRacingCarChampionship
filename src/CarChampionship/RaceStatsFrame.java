093fd6fd56cb2e28f4e8242dec079f1764292141
Chithara <chithara.2019160@iit.ac.lk>
2021-11-24 22:33:01 +0530

    added race frame class

--- src/CarChampionship/RaceStatsFrame.java
+++ src/CarChampionship/RaceStatsFrame.java
@@ -1,5 +1,18 @@
 package CarChampionship;
 
-public class RaceStatsFrame {
+import javax.swing.*;
+import java.awt.*;
 
+public class RaceStatsFrame {
+    JFrame tableFrame = new JFrame("Formula 1 Car Racing Championship");
+    JTable table;
+
+    public RaceStatsFrame(){
+
+        tableFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tableFrame.setSize(900, 700);
+        tableFrame.setLayout(new BorderLayout(0, 0));//add margin between components
+        ImageIcon image = new ImageIcon("images/CompanyName.png");//create an image icon for frame icon
+        tableFrame.setIconImage(image.getImage());//change icon of frame
+    }
 }
